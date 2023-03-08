b0e492ef8bc48fdb2bbeffaf1261b37918cd5eb1
Madhuka Priyashan <madhuka164@gmail.com>
2023-03-09 00:35:38 +0530

    login update

---
+++ ChatAPP/src/Login.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Login {
+    private JPanel panel1;
+    private JLabel chatFOTLabel;
+    private JButton LoginBtn;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
