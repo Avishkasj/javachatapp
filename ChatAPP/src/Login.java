19c8c60cb07a3bc7a6b4fbf43fb306d1e9e9ecaf
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-03-09 13:05:39 +0530

    login action

--- ChatAPP/src/Login.java
+++ ChatAPP/src/Login.java
@@ -1,11 +1,30 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-public class Login {
-    private JPanel panel1;
+public class Login extends JFrame{
+    JPanel p;
     private JLabel l;
     private JLabel close;
     private JLabel minimize;
-    private JLabel LoginBtn;
+    private JButton button1;
+
+    public Login() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(button1,"hello");
+            }
+        });
+        close.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.exit(0);
+            }
+        });
+    }
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
--- ChatAPP/src/Main.java
+++ ChatAPP/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        Login login = new Login();
+//        login.setContentPane(login.panel1);
+        login.setContentPane(login.p);
+        login.setVisible(true);
+        login.setSize(600,400);
+
     }
 }
