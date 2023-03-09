8f64960765c52059bfb78f73b47cf5d99b75a791
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-03-10 00:11:29 +0530

    admin

---
+++ ChatAPP/src/Admin.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Admin extends JFrame{
+    Admin(){
+        this.setContentPane(this.pp);
+        this.setVisible(true);
+        this.setSize(600,400);
+    }
+
+    JPanel pp;
+    //    private JPanel pp;
+}
--- ChatAPP/src/Main.java
+++ ChatAPP/src/Main.java
@@ -2,11 +2,18 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        Login login = new Login();
-//        login.setContentPane(login.panel1);
-        login.setContentPane(login.p);
-        login.setVisible(true);
-        login.setSize(600,400);
+//        Login login = new Login();
+////        login.setContentPane(login.panel1);
+//        login.setContentPane(login.p);
+//        login.setVisible(true);
+//        login.setSize(600,400);
+
+        Admin admin  = new Admin();
+
+
+
+
+
 
     }
 }
