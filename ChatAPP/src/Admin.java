421bfb0187ba876f3519e5ba62449abe3ed22d58
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-03-10 08:51:37 +0530

    admin exit

--- ChatAPP/src/Admin.java
+++ ChatAPP/src/Admin.java
@@ -1,10 +1,26 @@
 import javax.swing.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Admin extends JFrame{
     Admin(){
         this.setContentPane(this.pp);
         this.setVisible(true);
         this.setSize(600,400);
+        del1.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                super.componentResized(e);
+            }
+        });
+        exit.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.exit(0);
+            }
+        });
     }
 
     JPanel pp;
@@ -17,5 +33,15 @@ public class Admin extends JFrame{
     private JButton addButton3;
     private JButton removeButton3;
     private JButton createNewChatButton;
+    private JLabel del1;
+    private JLabel del2;
+    private JLabel exit;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
     //    private JPanel pp;
+
+
 }
+
