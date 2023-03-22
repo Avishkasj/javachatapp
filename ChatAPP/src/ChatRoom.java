32bdb8b3f9fdf9d516b0081291b4193aca91ff20
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-03-23 02:11:18 +0530

    create chat rooms

--- ChatAPP/src/ChatRoom.java
+++ ChatAPP/src/ChatRoom.java
@@ -1,8 +1,11 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
 
 public interface ChatRoom extends Remote {
     void subscribe(ChatClient client) throws RemoteException;
     void unsubscribe(ChatClient client) throws RemoteException;
     void broadcast(String message) throws RemoteException;
+    void createChatroom(String chatroomName) throws RemoteException;
+    List<String> getChatrooms() throws RemoteException;
 }
