60c2760eb3e299f0194991be0111c598246d4478
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-05-23 23:14:15 +0530

    Start chatapp v2

---
+++ ChatAppV2/src/main/java/com/chat/chatappv2/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.chat.chatappv2;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
