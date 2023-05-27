8fc70d099436bf2192c4efe0a5bb4e804535989f
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-05-27 15:17:55 +0530

    chat rooms

---
+++ ChatAppV2/src/main/java/com/chatapp/ui/register/Chatroom/Chatroom.java
@@ -0,0 +1,10 @@
+package com.chatapp.ui.register.Chatroom;
+
+import javax.swing.*;
+
+public class Chatroom {
+    private JTextField textField1;
+    private JButton createButton;
+    private JTable table1;
+    private JButton startButton;
+}
