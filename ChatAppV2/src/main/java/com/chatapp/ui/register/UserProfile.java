a8e5fffd91ae3591eaf9488e974983d442ce068f
Madhuka Priyashan <111411737+MadhukaJS@users.noreply.github.com>
2023-05-27 16:08:59 +0530

    UPDATE user profile

--- ChatAppV2/src/main/java/com/chatapp/ui/register/UserProfile.java
+++ ChatAppV2/src/main/java/com/chatapp/ui/register/UserProfile.java
@@ -3,14 +3,20 @@ package com.chatapp.ui.register;
 import com.chatapp.database.Database;
 import entity.UserEntity;
 
+import javax.swing.*;
 
 
 public class UserProfile {
+    private JPanel panel1;
+    private JTextField intID;
+    private JTextField textUname;
+    private JTextField textEmail;
+
     public static void main(String[] args) {
     Database database = new Database();
-    UserEntity user = database.UserPro(1); // Replace 123 with the specific user ID
+    UserEntity user = database.UserPro(3);
 
-        
+
         System.out.println("User ID: " + user.getId());
         System.out.println("Username: " + user.getUsername());
         System.out.println("Email: " + user.getEmail());
