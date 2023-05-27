2863d37d24d8dc0af583b7ee30aeacbc2e0131ba
Madhuka Priyashan <111411737+MadhukaJS@users.noreply.github.com>
2023-05-27 15:59:01 +0530

    user profile fixed

--- ChatAppV2/src/main/java/com/chatapp/ui/register/UserProfile.java
+++ ChatAppV2/src/main/java/com/chatapp/ui/register/UserProfile.java
@@ -3,17 +3,20 @@ package com.chatapp.ui.register;
 import com.chatapp.database.Database;
 import entity.UserEntity;
 
+
+
 public class UserProfile {
+    public static void main(String[] args) {
     Database database = new Database();
     UserEntity user = database.UserPro(1); // Replace 123 with the specific user ID
 
-if (user != null) {
+        
         System.out.println("User ID: " + user.getId());
         System.out.println("Username: " + user.getUsername());
         System.out.println("Email: " + user.getEmail());
-    }
-else {
         System.out.println("User not found!");
-    }
 
+    }
 }
+
+
